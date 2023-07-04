2d0b1c9272e3c8f75c13935f049a2bafbbe4fbbc
zhufucdev <1095291434@qq.com>
2023-07-04 18:24:57 +0800

    :lipstick: Add logo and title bar

--- app.vue
+++ app.vue
@@ -1,5 +1,14 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="bg-black text-white p-4">
+    <nav class="flex font-semibold">
+      <span>Brain0</span>
+    </nav>
+    <div class="flex justify-center items-center flex-row">
+      <div class="font-bold text-[100px] mr-20">
+        <p>Your brain, </p>
+        <p>Evolved</p>
+      </div>
+      <img src="public/brain0.transparent.png" class="logo" alt="Brain0 icon">
+    </div>
   </div>
 </template>
