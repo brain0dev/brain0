180aa0d5e2cc1613d00d9a13109a75afc0a06a0f
zhufucdev <1095291434@qq.com>
2023-07-07 21:45:04 +0800

    :boom: i18n

---
+++ i18n.config.ts
@@ -0,0 +1,20 @@
+export default defineI18nConfig(() => ({
+    legacy: false,
+    locale: 'en',
+    messages: {
+        en: {
+            brand: "Brain0",
+            title1: "Your Brain,",
+            title2: "Evolved",
+            get_started: "Get Started",
+            about: "About"
+        },
+        zh: {
+            brand: "零号大脑",
+            title1: "进化",
+            title2: "你的大脑，",
+            get_started: "开始使用",
+            about: "关于"
+        }
+    }
+}))
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/tailwindcss'],
-  devtools: { enabled: true }
+  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/i18n'],
+  devtools: { enabled: true },
+  i18n: {
+    locales: ['en', 'zh'],
+    defaultLocale: 'en',
+    detectBrowserLanguage: {
+      useCookie: true,
+      cookieKey: 'i18n_redirected',
+      redirectOn: 'root',
+    }
+  }
 })
