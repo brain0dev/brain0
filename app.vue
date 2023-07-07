775782812ad3158e157fd5b748838315208fe077
zhufucdev <1095291434@qq.com>
2023-07-07 21:45:14 +0800

    :boom: Use vue router

--- app.vue
+++ app.vue
@@ -1,14 +1,7 @@
 <template>
-  <div class="bg-black text-white p-4">
-    <nav class="flex font-semibold">
-      <span>Brain0</span>
-    </nav>
-    <div class="flex justify-center items-center flex-row">
-      <div class="font-bold text-[100px] mr-20">
-        <p>Your brain, </p>
-        <p>Evolved</p>
-      </div>
-      <img src="public/brain0.transparent.png" class="logo" alt="Brain0 icon">
-    </div>
+  <div>
+    <NuxtLayout>
+      <NuxtPage/>
+    </NuxtLayout>
   </div>
 </template>
