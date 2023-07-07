df097f2c3729e9fea00037169fa22fa58f0a444f
zhufucdev <1095291434@qq.com>
2023-07-07 21:46:00 +0800

    :lipstick: Use components

---
+++ components/NavigationBar.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <nav class="flex font-semibold text-2xl p-2">
+    <span>{{ $t('brand') }}</span>
+  </nav>
+</template>
+
+<style scoped>
+
+</style>
